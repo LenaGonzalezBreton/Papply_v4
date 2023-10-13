92e7d44edce28f7beeb94aa89fbaa510659e481a
LenaGB <lena.gonzalezbreton@proton.me>
2023-10-13 11:47:45 +0200

    Add models

---
+++ Models/Degree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapplyAppli.Classes
+{
+    internal class Degree
+    {
+        // attributs
+        private int _idDegree { get; set; } 
+        private string _libDegree { get; set; }
+
+        // constructeur
+        public Degree(int idDegree, string libDegree )
+        {
+            _idDegree = idDegree;
+            _libDegree = libDegree;
+        }
+
+        // accesseurs
+
+        // methodes
+
+
+
+    }
+}
---
+++ Views/TP/GestionTp.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Papply.Views.TP;
+
+public partial class GestionTp : UserControl
+{
+    public GestionTp()
+    {
+        InitializeComponent();
+    }
+}
