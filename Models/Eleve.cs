53acf82e04c695a9ecf1b8cd087165d070e7aeb9
Sword25000 <lecoqc39@hotmail.fr>
2023-11-20 17:07:45 +0100

    suite dashboard

---
+++ Models/Eleve.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Papply.Models
+{
+    internal class Eleve
+    {
+        public int IdEleve { get; set; }
+        public string NomEleve { get; set; }
+        public string PrenomEleve { get; set; }
+        public string MailEleve { get; set; }
+        public string PasswordEleve { get; set; }
+
+    }
+}
