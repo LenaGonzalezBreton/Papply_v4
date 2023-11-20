729dac0de11446a655fa4bdebfa65cfb76bb9739
Sword25000 <lecoqc39@hotmail.fr>
2023-11-20 10:01:25 +0100

    Début dashboard

---
+++ Views/DashBoard.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using Avalonia;
+using Avalonia.Markup.Xaml;
+
+
+namespace Papply.Views
+{
+    public partial class DashBoard : UserControl
+    {
+        public DashBoard()
+        {
+            InitializeComponent();
+        }
+    }
+}
